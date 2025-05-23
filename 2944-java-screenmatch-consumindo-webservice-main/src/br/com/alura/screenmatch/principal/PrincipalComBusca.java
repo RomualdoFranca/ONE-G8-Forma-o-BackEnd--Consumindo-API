daf99305c452c6fee1913d2e5878bde99206434c
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-05-23 20:54:22 -0300

    implementa a URL dinamicamente para escolher o filme da pesquisa pelo scanner

--- 2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ 2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -10,10 +10,14 @@ import java.util.Scanner;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-         
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome do filme para busca:");
+        var busca = leitura.next();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=f896c64f";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=ghost+busters&apikey=f896c64f"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
